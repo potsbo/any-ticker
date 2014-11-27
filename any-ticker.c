6563748d848737da81e80248cad2feb04939c386
Shimpei Otsubo <potsbo@gmail.com>
2014-11-27 02:25:59 +0000

    eaters can be put depending on the number of guns, source of objects added

--- any-ticker.c
+++ any-ticker.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
 int append( char *inputFileName, char *outputFileName, int shiftX, int shiftY, int yDirection);
 int outputFileInitialise( char *outputFileName);
 int debugFlag = 0;
 
 int main(int argc, char *argv[]){
-	int yEaterCentre = 79;
-	int xEaterCentre = 18;
+	int yEatersCentre = 79;
+	int xEatersCentre = 18;
+	int yEaterCentre = -11;
+	int xEaterCentre = 4;
 	int yGunCentre = 292;
 	int xGunCentre = -5; //Can't be closer than this
 	
 	char eaterFileName[100];
 	/* set file name here */
-	strcpy( eaterFileName, "ticker.eater.life");
+	/* strcpy( eaterFileName, "ticker.eater.life"); */
+	strcpy( eaterFileName, "eater.life");
 
 	char outputFileName[100];
 	/* set file name here */	
@@ -25,14 +29,9 @@ int main(int argc, char *argv[]){
 	/* set file name here */
 	strcpy( gunFileName, "ticker.gun.life");
 	
-	int gunNum = 7;
+	int gunNum = 9;
 	for(int i = 0; i < gunNum; i++){
-		int yFlag = 1;
-		if(i % 2 == 0){
-			yFlag = 1;
-		}else{
-			yFlag = -1;
-		}
+		int yFlag = pow(-1,i );
 		if( append( gunFileName, outputFileName, -xGunCentre - 115 * (i/2) , -yGunCentre - 18 * (i/2), yFlag) != 0 ){ 
 			printf("Error in function \"append\". abort\n");
 			return 1;
@@ -40,13 +39,24 @@ int main(int argc, char *argv[]){
 
 	}
 	printf("%d gun(s) put\n", gunNum);
+
 	/* putting eaters */
 	int distant = 0;
 	distant += 115 *  ( (gunNum - 1) / 2) - ((gunNum -1) / 2) % 2;
-	if( append( eaterFileName, outputFileName, -xEaterCentre - distant, -yEaterCentre, -1) != 0 ){ 
-		printf("Error in function \"append\". abort\n");
-		return 1;
+	int eaterNum = gunNum + 2;
+	for( int i = 0; i < eaterNum; i++){
+		int yFlag = pow( -1, ( i+3)/2);
+		int negFlag =1;
+		if((i+3) % 4 == 0 || i % 4 == 0){ negFlag = -1; }
+		if( append( eaterFileName, outputFileName, -xEaterCentre - distant, -yEaterCentre - negFlag * 36 * ( (i + 2)/4), yFlag) != 0 ){ 
+			printf("Error in function \"append\". abort\n");
+			return 1;
+		}
 	}
+	/* if( append( eaterFileName, outputFileName, -xEatersCentre - distant, -yEatersCentre, 1) != 0 ){ */ 
+	/* 	printf("Error in function \"append\". abort\n"); */
+	/* 	return 1; */
+	/* } */
 	printf("Eaters are put\n");
 
 	printf("End combining with no error\n");
