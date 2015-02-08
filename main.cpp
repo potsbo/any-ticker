89cc94926a6a2701adbc86fa5c51038b69886cb4
Shimpei Otsubo <potsbo@gmail.com>
2015-02-08 18:21:37 +0000

    main.cpp in more like c++, add error prompt when non-supported command is inputed

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include "stdafx.h"
 #include "any-ticker.h"
+#include <iostream>
+
+using namespace std;
 
 /* for errors */
 int debugFlag = 0;
@@ -9,8 +12,11 @@ int main( int argc, char *argv[]){
 	if( argc > 1){
 		if( strcmp( argv[1], "ticker") == 0)
 			any_ticker( argc-1, argv+1);
+		else{
+			cout << "Error: No command \"" << argv[1] << "\""<< endl;
+		}
 	}else{
-		printf("TODO: print help text\n");
+		cout << "TODO: print help text" << endl;
 	}
 	return 0;
 }
