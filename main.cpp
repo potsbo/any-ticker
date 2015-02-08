847403e23ac1b62982d4535c1ba619c27818b06a
Shimpei Otsubo <potsbo@gmail.com>
2015-02-08 18:29:32 +0000

    help text support

--- main.cpp
+++ main.cpp
@@ -8,15 +8,26 @@ using namespace std;
 int debugFlag = 0;
 int errorNum = 0;
 
+void help( void);
+
 int main( int argc, char *argv[]){
 	if( argc > 1){
 		if( strcmp( argv[1], "ticker") == 0)
 			any_ticker( argc-1, argv+1);
-		else{
+		else if( strcmp (argv[1], "help")){
+			help();
+		}else{
 			cout << "Error: No command \"" << argv[1] << "\""<< endl;
+			help();
 		}
 	}else{
-		cout << "TODO: print help text" << endl;
+		help();
 	}
 	return 0;
+}
+
+void help( void){
+	cout << "Command list:" << endl;
+	cout << "ticker - make a ticker message" << endl;
+	cout << "help - show this text" << endl;
 }
