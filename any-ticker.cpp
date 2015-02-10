60feae9608f3d27b595b60d3b90774d81a75090d
Shimpei Otsubo <potsbo@gmail.com>
2015-02-10 11:06:19 +0000

    simpler LifeObject::install

--- any-ticker.cpp
+++ any-ticker.cpp
@@ -14,7 +14,7 @@ const int X_MAX = 1024;
 const int Y_MAX = 256;
 
 /* interesting parameters */
-const int X_DOT_SHIFT = 5; // can't be less than 4
+const int X_DOT_SHIFT = 5; 	// can't be less than 4
 const int Y_UNIT = 18;		// must be 18, otherwise cause bug, which should be fixed
 
 class LifeObject{
@@ -22,14 +22,18 @@ class LifeObject{
 		string fileNameRoot;
 		int xCentre; int yCentre; int phase; int direction;
 		string genFileName( string path);
+		static string outputFileName;
 	public:
 		static int xShift, yShift;
 		void Set( string f, int x, int y, int p, int d);
-		void install( const char *of, int shiftX, int shiftY, int yDirection);
+		void install( int shiftX, int shiftY, int yDirection);
+		static void setOutputFile( string);
 };
 int LifeObject::xShift, LifeObject::yShift;
+string LifeObject::outputFileName;
 
-int installGliders( LifeObject *glider, int dots[][256], int xAreaSize, int delShift, int yAreaSize, const char *of);
+int installGliders( LifeObject *, int [][256], int, int, int, const char*);
+
 
 int any_ticker(int argc, char *argv[]){
 
@@ -112,6 +116,8 @@ int any_ticker(int argc, char *argv[]){
 	galaxy[5].Set("galaxy",  19, -4, 5, 0);
 	galaxy[6].Set("galaxy",  18, -5, 6, 0);
 	galaxy[7].Set("galaxy",  18, -5, 7, 0);
+
+	LifeObject::setOutputFile( outputFileName);
     
 	/* setting the dot map */
 	/* reading font file and ticker message */
@@ -137,7 +143,7 @@ int any_ticker(int argc, char *argv[]){
 
 	/* set dots in blank space to zero */
 	for(int y = 0; y < yAreaSize; y++)
-		for(int x = 0; x < xAreaSize - xLeastAreaSize; x++)
+		for(int x = 0; x < xAreaSize -xLeastAreaSize; x++)
 			dots[xLeastAreaSize +x][y] = 0;
 
 
@@ -183,9 +189,9 @@ int any_ticker(int argc, char *argv[]){
 			int shpNum = uselessDots /2; 	// one ship deletes 2 gliders
 			int blkNum = uselessDots %2;	// one block deletes 1 glider
 			for( int i = 0; i < shpNum; i++)
-				shp.install( outputFileName.c_str(), -i*4, -i*4, yFlag);
+				shp.install( -i*4, -i*4, yFlag);
 			if( blkNum == 1)
-				blk.install( outputFileName.c_str(), -shpNum*4, -shpNum*4, yFlag);
+				blk.install( -shpNum*4, -shpNum*4, yFlag);
 			/* end of a row */
 		}
 		if( debugFlag != 0) cout << "delMax: " << delMax << endl;
@@ -206,11 +212,11 @@ int any_ticker(int argc, char *argv[]){
 		int refShift = (xAreaSize -5) /4;			// reflector shift depens on xAreaSize
 
 		/* guns */
-		dup.install( outputFileName.c_str(), -delShift *PERIOD, -delShift *PERIOD, yFlag);
-		lws.install( outputFileName.c_str(), 0, 0, yFlag);
+		dup.install( -delShift *PERIOD, -delShift *PERIOD, yFlag);
+		lws.install( 0, 0, yFlag);
 
 		/* reflectors */
-		ref.install( outputFileName.c_str(), +PERIOD*refShift -delShift *PERIOD,
+		ref.install( +PERIOD*refShift -delShift *PERIOD,
 				-PERIOD*refShift -delShift *PERIOD, yFlag);
 	}
 	cout << gunNum;
@@ -226,7 +232,7 @@ int any_ticker(int argc, char *argv[]){
 	for( int i = 0; i < eaterNum; i++){
 		int yFlag = pow( -1, (i+3)/2);
 		int negFlag = pow( -1, (i+2)/2);
-		eat.install( outputFileName.c_str(), -distance,
+		eat.install( -distance,
 				-negFlag * 2*Y_UNIT * ( (i + 2)/4), yFlag);
 	}
 	cout << eaterNum << " eaters installed" << endl;
@@ -240,7 +246,7 @@ int any_ticker(int argc, char *argv[]){
 		while(y < 0) y +=8;
 
 		/* galaxies on the left of eaters */
-		galaxy[y%8].install( outputFileName.c_str(), -distance, 18*i, 1);
+		galaxy[y%8].install(  -distance, 18*i, 1);
 
 		/* calculating which galaxy to have to make it a temporary eater */
 		int firstLive = xAreaSize; 
@@ -262,10 +268,10 @@ int any_ticker(int argc, char *argv[]){
 			genToGlx += delShift *4;
 			if( ( (y + ( gunNum+1)/2)%2) %2 != 0)
 				/* want to make this simple */
-				galaxy[(genToGlx)%8].install( outputFileName.c_str(),
+				galaxy[(genToGlx)%8].install( 
 						-distance+24, 18*i, 1);
 			else
-				galaxy[(genToGlx+6)%8].install( outputFileName.c_str(),
+				galaxy[(genToGlx+6)%8].install( 
 						-distance+23, 18*i, 1);
 		}
 		/* end of a row */
@@ -296,24 +302,24 @@ int installGliders( LifeObject *glider, int dots[][256], int xAreaSize, int delS
 		/* gliders */
 		for( int i = 0; i < refShift +1; i++){
 			if( dots[dotShift(i*2,shiftNum,xAreaSize)][y] == 1)
-				glider[0].install( of, +PERIOD*i, -PERIOD*i, yFlag);
+				glider[0].install( +PERIOD*i, -PERIOD*i, yFlag);
 			if( dots[dotShift(xAreaSize - 2 - 2*i,shiftNum,xAreaSize)][y] == 1)
-				glider[4].install( of, +PERIOD*i, -PERIOD*i, yFlag);
+				glider[4].install( +PERIOD*i, -PERIOD*i, yFlag);
 		}
 
 		for( int i = 0; i < refShift; i++){
 			if( dots[dotShift( i*2 + 1,shiftNum,xAreaSize)][y] == 1)
-				glider[5].install( of, +PERIOD*i, -PERIOD*i, yFlag);
+				glider[5].install( +PERIOD*i, -PERIOD*i, yFlag);
 			if( dots[dotShift( xAreaSize - 3 - 2*i,shiftNum,xAreaSize)][y] == 1)
-				glider[6].install( of, +PERIOD*i, -PERIOD*i, yFlag);
+				glider[6].install( +PERIOD*i, -PERIOD*i, yFlag);
 		}
 
 		if( dots[dotShift( 2 -1 +2*refShift,shiftNum,xAreaSize)][y] == 1)
-			glider[3].install( of,  +PERIOD*refShift, -PERIOD*refShift, yFlag);
+			glider[3].install( +PERIOD*refShift, -PERIOD*refShift, yFlag);
 		if( dots[dotShift( 3 -1 +2*refShift,shiftNum,xAreaSize)][y] == 1)
-			glider[2].install( of,  +PERIOD*refShift,-PERIOD*refShift, yFlag);
+			glider[2].install( +PERIOD*refShift,-PERIOD*refShift, yFlag);
 		if( dots[dotShift( 5 -1 +4*refShift,shiftNum,xAreaSize)][y] == 1)
-			glider[1].install( of, 0, 0, yFlag);
+			glider[1].install( 0, 0, yFlag);
 
 	}
 	return 0;
@@ -328,15 +334,15 @@ void LifeObject::Set( std::string f, int x, int y, int p, int d){
 }
 
 string LifeObject::genFileName( std::string path){
-	std::string FileName;
+	string FileName;
 	FileName = path;
 	FileName += fileNameRoot + ".";
-	FileName += std::to_string(phase) + ".";
-	FileName += std::to_string(direction) + ".life";
+	FileName += to_string(phase) + ".";
+	FileName += to_string(direction) + ".life";
 	return FileName;
 }
 
-void LifeObject::install(const char *of, int shiftX, int shiftY, int yDirection){
+void LifeObject::install( int shiftX, int shiftY, int yDirection){
 	shiftX -= xShift;
 	shiftY -= yShift;
 
@@ -359,14 +365,14 @@ void LifeObject::install(const char *of, int shiftX, int shiftY, int yDirection)
 
 	/* opening output file */
 	FILE *outputFile;
-	outputFile = fopen( of, "a");
+	outputFile = fopen( outputFileName.c_str(), "a");
 
 	/* checking the file */
 	if( outputFile == NULL){
-		cout << "Can't open " << of << endl;
+		cout << "Can't open " << outputFileName << endl;
 		errorNum++;
 	}else{
-		if( debugFlag != 0) cout << of << "is successfully opened" << endl;
+		if( debugFlag != 0) cout << outputFileName << "is successfully opened" << endl;
 	}
 
 	char tempString[X_MAX];
@@ -388,3 +394,6 @@ void LifeObject::install(const char *of, int shiftX, int shiftY, int yDirection)
 	fclose( outputFile);
 }
 
+void LifeObject::setOutputFile( string FileName){
+	LifeObject::outputFileName = FileName;
+}
