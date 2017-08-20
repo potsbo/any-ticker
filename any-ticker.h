c450f9c9b9487f2887a4b164495fd217a7971187
Shimpei Otsubo <potsbo@gmail.com>
2017-08-21 02:47:16 +0900

    fix namespace

--- any-ticker.h
+++ any-ticker.h
@@ -17,10 +17,10 @@ class LifeObject{
 	private:
 		std::string fileNameRoot;
 		int xCentre; int yCentre; int phase; int direction;
-		string genFileName( string path);
+		std::string genFileName( std::string path);
 	public:
 		static int xShift, yShift;
-		void Set( string f, int x, int y, int p, int d);
+		void Set( std::string f, int x, int y, int p, int d);
 		void install( const char *of, int shiftX, int shiftY, int yDirection);
 };
 int LifeObject::xShift, LifeObject::yShift;
