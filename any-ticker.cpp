ee3f7ecb251540563af1a9b0a63aa1c45b075beb
Shimpei Otsubo <potsbo@gmail.com>
2017-08-21 03:00:08 +0900

    Never use common

--- any-ticker.cpp
+++ any-ticker.cpp
@@ -80,7 +80,7 @@ int any_ticker(int argc, char *argv[]){
 
 	/* setting objects */
 	/* {type, xCentre, yCentre, phase, direction} */
-	LifeObject eat, dup, lws, ref, shp, blk, common, glider[7], galaxy[8];
+	LifeObject eat, dup, lws, ref, shp, blk, glider[7], galaxy[8];
 	eat.Set(       "eater",   4,-11, 0, 0);
 	dup.Set(  "duplicator",  -5, 88, 0, 0);
 	lws.Set(   "lwssmaker", -83, 31, 0, 0);
@@ -153,9 +153,9 @@ int any_ticker(int argc, char *argv[]){
 		int shipNum = 0;	// one ship can delete two dots(gliders)
 		for( int i = 0; i < gunNum; i++){
 			/* each row */
-			common.xShift = X_DOT_SHIFT *PERIOD *(i/2);
+			LifeObject::xShift = X_DOT_SHIFT *PERIOD *(i/2);
 			// guns and reflectors shifted by this
-			common.yShift = Y_UNIT *(i/2);
+			LifeObject::yShift = Y_UNIT *(i/2);
 			int yFlag = pow(-1, i); // make object upside down
 			int uselessDots = 0;	// the number of useless dots
 			int y = ( gunNum -yFlag *i +i%2)/2;
@@ -191,8 +191,8 @@ int any_ticker(int argc, char *argv[]){
 	/* guns and reflectors */
 	for(int i = 0; i < gunNum; i++){
 		int yFlag = pow(-1, i);						// make object upside down
-		common.xShift = X_DOT_SHIFT *PERIOD *(i/2);	// guns and reflectors shifted by this
-		common.yShift = Y_UNIT *(i/2);
+		LifeObject::xShift = X_DOT_SHIFT *PERIOD *(i/2);	// guns and reflectors shifted by this
+		LifeObject::yShift = Y_UNIT *(i/2);
 		int refShift = (xAreaSize -5) /4;			// reflector shift depens on xAreaSize
 
 		/* guns */
@@ -212,7 +212,7 @@ int any_ticker(int argc, char *argv[]){
 	/* installing eaters */
 	//eaters shifted because of the number of guns
 	int eaterNum = gunNum + abs(extraEaters);
-	common.xShift = common.yShift = 0;
+  LifeObject::xShift = LifeObject::yShift = 0;
 	for( int i = 0; i < eaterNum; i++){
 		int yFlag = pow( -1, (i+3)/2);
 		int negFlag = pow( -1, (i+2)/2);
@@ -276,9 +276,8 @@ int installGliders( LifeObject *glider, int dots[][256], int xAreaSize, int delS
 
 	for(int i = 0; i < gunNum; i++){
 		int yFlag = pow(-1, i); // make object upside down
-		LifeObject common;
-		common.xShift = ( X_DOT_SHIFT *(i/2) + delShift)*PERIOD;// gliders shifted by this
-		common.yShift = Y_UNIT *(i/2) + delShift *PERIOD;
+		LifeObject::xShift = ( X_DOT_SHIFT *(i/2) + delShift)*PERIOD;// gliders shifted by this
+		LifeObject::yShift = Y_UNIT *(i/2) + delShift *PERIOD;
 		int shiftNum = i;
 		int refShift = (xAreaSize -5) /4; // reflector shifted depending on xAreaSize
 		int y = ( gunNum -yFlag *i +i%2)/2;
