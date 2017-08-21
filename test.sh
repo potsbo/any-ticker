f8370c6013190cf5a1161fa05889315677807a54
Shimpei Otsubo <potsbo@gmail.com>
2017-08-21 11:48:19 +0900

    comprehensive test

--- test.sh
+++ test.sh
@@ -5,9 +5,18 @@ set -e
 perform_test() {
   local file=$1
   shift
-  ./life ticker $@
-  diff any-ticker.life ./test/ticker/$file
+  ./life ticker $@ > /dev/null
+  if diff any-ticker.life ./test/ticker/$file; then
+    echo "$file ok"
+  else
+    echo "$file failed"
+    exit 1
+  fi
 }
 
+echo "Ticker test cases"
 perform_test 001
 perform_test 002 -m "abcdefghijklmnopqrstuvwxyz"
+
+echo ""
+echo "Finished successfully"
