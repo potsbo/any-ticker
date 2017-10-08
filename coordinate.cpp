6270296add486d39f8dc5b8bafd95d2b88c45965
Shimpei Otsubo <potsbo@gmail.com>
2017-10-08 19:11:55 +0900

    Remove std from coordinate.cpp

--- coordinate.cpp
+++ coordinate.cpp
@@ -1,9 +1,11 @@
 #include "stdafx.h"
 #include "coordinate.h"
 
-std::string Coordinate::to_str() {
-	std::string xs = std::to_string(x);
-	std::string ys = std::to_string(y);
+using namespace std;
+
+string Coordinate::to_str() {
+	string xs = to_string(x);
+	string ys = to_string(y);
 	return xs + " " + ys + "\n";
 }
 
