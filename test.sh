9ce9c2f538a64f2443fc7ea218aedc8516f1e4fd
Shimpei Otsubo <potsbo@gmail.com>
2017-10-08 21:26:55 +0900

    Add convert test cases

--- test.sh
+++ test.sh
@@ -14,9 +14,26 @@ perform_test() {
   fi
 }
 
+test_convert() {
+  local number=$1
+  shift
+  ./life ticker $@ > /dev/null
+  ./life convert any-ticker.life > output.rle
+  if diff output.rle ./test/convert/$number; then
+    echo "$file ok"
+  else
+    echo "$file failed"
+    exit 1
+  fi
+}
+
 echo "Ticker test cases"
 perform_test 001
 perform_test 002 -m "abcdefghijklmnopqrstuvwxyz"
-
 echo ""
+
+echo "Convert test cases"
+perform_test 001
+echo ""
+
 echo "Finished successfully"
