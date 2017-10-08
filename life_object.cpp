3cb01b7eb690227f81668961124bb1efd69532c9
Shimpei Otsubo <potsbo@gmail.com>
2017-10-08 16:52:35 +0900

    Fix

--- life_object.cpp
+++ life_object.cpp
@@ -1,4 +1,5 @@
 #include "stdafx.h"
+#include "coordinate.h"
 #include "life_object.h"
 
 using namespace std;
--- life_object.h
+++ life_object.h
@@ -1,15 +1,18 @@
 #ifndef _LIFE_OBJECT_H_
 #define _LIFE_OBJECT_H_
+#include "coordinate.h"
 
-class Coordinate{
+class LifeObject{
+	private:
+		std::string fileNameRoot;
+		int xCentre; int yCentre; int phase; int direction;
+		std::string genFileName( std::string path);
 	public:
-		Coordinate(int newX, int newY) {
-			x = newX;
-			y = newY;
-		}
-		int x;
-		int y;
-		std::string to_str();
+		static int xShift, yShift, yFlag;
+		static std::vector<Coordinate> outputDots;
+		void Set( std::string f, int x, int y, int p, int d);
+		std::vector<Coordinate> install(int shiftX, int shiftY);
+		static void write();
 };
 
 #endif // _LIFE_OBJECT_H_
